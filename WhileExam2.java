cc37f00aa4de4946996ee487d587b4a560f37563
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-08-21 15:11:10 +0900

    [while] 1부터 10까지의 숫자 중 홀수는 더하고 짝수는 빼기

---
+++ WhileExam2.java
@@ -0,0 +1,12 @@
+public class WhileExam2 {
+	public static void main( String args[ ] ) {
+		int i = 1, sum = 0;
+		System.out.print("1-2+3-4+5-6+7-8+9-10 = ");
+		while ( i <= 10 ) {
+			if ( i % 2 == 0) sum -= i;
+			else sum +=i;
+			i++;
+		}
+		System.out.print(sum);
+	}
+}
