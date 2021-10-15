73badc6b7c092bb74a0eac8e461f93f3233d228e
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-10-15 09:42:36 +0900

    [array] 각 조의 점수를 합산해 가장 높은 조를 출력
    (+) 꾸미기

--- Array2_score.java
+++ Array2_score.java
@@ -11,18 +11,14 @@ public class Array2_score {
 		String[] sub_title = {"완성도", "창의성", "이해도"};
 		
 		for(int i = 0; i < score.length; i++) {
-			System.out.println("- " + (i+1) + "번째 조 -");
+			System.out.println("- " + (i+1) + "조 -");
 			for(int j = 0; j < score[i].length; j++) {
-				System.out.print(sub_title[j] + " : ");
-				score[i][j] = sc.nextInt();
-				if(!(score[i][j] >= 0 && score[i][j] <= 20))
-					j--;
-//				while(true) {
-//					System.out.print(sub_title[j] + " : ");
-//					score[i][j] = sc.nextInt();
-//					if(score[i][j] >= 0 && score[i][j] <= 20)
-//						break;
-//				}
+				while(true) {
+					System.out.print(sub_title[j] + " : ");
+					score[i][j] = sc.nextInt();
+					if(score[i][j] >= 0 && score[i][j] <= 20)
+						break;
+				}
 				add += score[i][j];
 				if(add > max) {
 					max = add;
@@ -33,6 +29,20 @@ public class Array2_score {
 			add = 0;
 		}
 		
-		System.out.print(win + "조 : " + max);
+		System.out.println("=======================================");
+		for(int i = 0; i < sub_title.length; i++) 
+				System.out.print("\t" + sub_title[i]);
+		System.out.println();
+		System.out.println("=======================================");
+		
+		for(int i = 0; i < score.length; i++) {
+			System.out.print((i+1) + "조 : \t");
+			for(int j = 0; j < score[i].length; j++) 
+				System.out.print(score[i][j] + "\t");
+			System.out.println();
+		}
+		System.out.println("=======================================");
+		System.out.print("최고팀 : " + win + "조");
+		//System.out.print(win + "조 : " + max);
 	}
 }
