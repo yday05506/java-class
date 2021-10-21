73f33af737f82acc3de026b808bc0d65fee05df4
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-10-21 16:05:00 +0900

    [array] 명령행 매개변수

---
+++ ArguExam.java
@@ -0,0 +1,7 @@
+public class ArguExam {
+	public static void main( String args[ ] ) {
+		System.out.println("입력된 문자열 개수 : "+ args.length);
+		for(int i = 0; i < args.length; i++)
+			System.out.print(args[i] + "\t");
+	}
+}
