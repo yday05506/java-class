fde6df61f1db7ba44f0267489b3336af95313079
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-12-03 00:40:29 +0900

    [class] 상자 두 개의 부피 구하기

--- TwoBox.java
+++ TwoBox.java
@@ -3,8 +3,8 @@ package Class;
 public class TwoBox {
 
 	public static void main(String[] args) {
-		Box B1 = new Box(1, 2, 3);
-		Box B2 = new Box(4, 5, 6);
+		Box11 B1 = new Box11(1, 2, 3);
+		Box11 B2 = new Box11(4, 5, 6);
 		
 		B1.volume(2);
 		System.out.println();
@@ -13,12 +13,12 @@ public class TwoBox {
 
 }
 
-class Box {
+class Box11 {
 	int width;
 	int height;
 	int depth;
 	
-	Box (int wi, int he, int dep) {
+	Box11 (int wi, int he, int dep) {
 		width = wi;
 		height = he;
 		depth = dep;
@@ -32,7 +32,7 @@ class Box {
 		return width*height*depth*t;
 	}
 	
-	void volume(int times) {
+	public void volume(int times) {
 		System.out.println("가로 : " + width);
 		System.out.println("세로 : " + height);
 		System.out.println("높이 : " + depth);
