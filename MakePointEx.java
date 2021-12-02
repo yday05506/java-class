9479ca4277ed74fe050c3d465ad42db7625307ad
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-12-03 00:42:06 +0900

    [class] 점의 좌표를 입력 받아 이동시키고 이동한 점의 좌표 구하기

--- MakePointEx.java
+++ MakePointEx.java
@@ -6,18 +6,29 @@ public class MakePointEx {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Point P1 = new Point(0, 0);
+		Point1 P1 = new Point1(0, 0);
+		System.out.print("처음 x값 입력 : ");
+		int x = sc.nextInt();
+		System.out.print("처음 y값 입력 : ");
+		int y = sc.nextInt();
+		
+		System.out.print("이동 할 x값 입력 : ");
+		int xx = sc.nextInt();
+		int x1 = P1.x + xx;
+		System.out.print("이동 할 y값 입력 : ");
+		int yy = sc.nextInt();
+		int y1 = P1.y + yy;
+		
+		System.out.println("x, y의 좌표 : " + x1 + ", " + y1);
 	}
 
 }
 
-class Point {
-	Scanner sc = new Scanner(System.in);
-	
+class Point1 {
 	int x;
 	int y;
 	
-	Point(int xx, int yy) {
+	Point1(int xx, int yy) {
 		x = xx;
 		y = yy;
 	}
